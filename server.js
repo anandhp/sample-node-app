b64253c1747b227e44abe61afec3b591fc278669
Anand Patil <anand_patil@intuit.com>
2017-09-30 01:29:11 +0530

    Sample changes to test docker auto build and deploy

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+  res.send('This is the sample node application in docker swarm\n');
 });
 
 app.listen(PORT, HOST);
